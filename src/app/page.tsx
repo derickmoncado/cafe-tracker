6a9c92136ae85d1551b5dedf818bb5508a830695
Derick Moncado <Derick.Moncado@activision.com>
2025-08-02 15:30:53 -0700

    Set up and got Sass working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
 	return (
 		<div className="container">
-			<h1>Welcome to Cafe Tracker ayyyyy</h1>
+			<h1 className="heading-test">Welcome to Cafe Tracker ayyyyy</h1>
+			<button className="button-test">test button</button>
+			<p className={styles["paragraph-test"]}>
+				This is a test paragraph.
+			</p>
+			<div className="test-container">
+				<p className="test-paragraph">This is a test paragraph.</p>
+				<p className="test-paragraph">
+					This is another test paragraph.
+				</p>
+			</div>
 		</div>
 	);
 }
