2e40450ea6086885dcabffb8f07ce3118757ea36
Derick Moncado <Derick.Moncado@activision.com>
2025-08-18 16:18:29 -0700

    Moved styles outside of /app, started styling body header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,23 @@ import styles from "./page.module.scss";
 export default function Home() {
 	return (
 		<div className="container">
-			<h1 className="heading-test">All Entries</h1>
+			<div className={styles["page-header"]}>
+				<div className={styles["page-header__title"]}>
+					<h2>All Entries</h2>
+					<div className={styles["page-header__title__filters"]}>
+						filters go here
+					</div>
+				</div>
+
+				<div className={styles["page-header__user"]}>
+					<div className={styles["page-header__user__profile"]}>
+						user stuff here
+					</div>
+					<div className={styles["page-header__user__search"]}>
+						search goes here
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
