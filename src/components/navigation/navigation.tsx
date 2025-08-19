f1d7dc4171177b5c7e5b47fa3b2b8170a9b154ad
Derick Moncado <Derick.Moncado@activision.com>
2025-08-18 17:13:17 -0700

    Styled Navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import React from "react";
 import styles from "./navigation.module.scss";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Coffee, Martini, Rows4, Utensils } from "lucide-react";
 
 export default function Navigation() {
+	const pathname = usePathname();
+
 	return (
 		<nav className={styles.navigation}>
 			<div className={styles.logo}>
@@ -13,16 +19,48 @@ export default function Navigation() {
 			</div>
 			<ul>
 				<li>
-					<Link href="/">All Entries</Link>
+					<Link
+						href="/"
+						className={pathname === "/" ? styles.active : undefined}
+					>
+						<Rows4 /> All Entries
+					</Link>
 				</li>
 				<li>
-					<Link href="/coffee-shops">Coffee Shops</Link>
+					<Link
+						href="/coffee-shops"
+						className={
+							pathname === "/coffee-shops"
+								? styles.active
+								: undefined
+						}
+					>
+						<Coffee /> Coffee Shops
+					</Link>
 				</li>
 				<li>
-					<Link href="/bars-lounges">Bars & Lounges</Link>
+					<Link
+						href="/bars-lounges"
+						className={
+							pathname === "/bars-lounges"
+								? styles.active
+								: undefined
+						}
+					>
+						<Martini /> Bars & Lounges
+					</Link>
 				</li>
 				<li>
-					<Link href="/restaurants">Restaurants</Link>
+					<Link
+						href="/restaurants"
+						className={
+							pathname === "/restaurants"
+								? styles.active
+								: undefined
+						}
+					>
+						<Utensils /> Restaurants
+					</Link>
 				</li>
 			</ul>
 		</nav>
