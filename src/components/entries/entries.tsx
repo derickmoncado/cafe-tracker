17a746a02e7dec569ed288b92b94bdaa8c9f1073
Derick Moncado <Derick.Moncado@activision.com>
2025-08-22 19:47:43 -0700

    Styling the entries

--- src/components/entries/entries.tsx
+++ src/components/entries/entries.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./entries.module.scss";
+import { Star } from "lucide-react";
 
 export default function Entries() {
 	return (
@@ -8,7 +9,32 @@ export default function Entries() {
 				<summary>
 					<h2>Entries</h2>
 				</summary>
-				<p>[ entry list goes here... table??]</p>
+
+				<div className={styles["entry-headings"]}>
+					<span className={styles["name"]}>Name</span>
+					<span className={styles["status"]}>Status</span>
+					<span className={styles["address"]}>Address</span>
+					<span className={styles["date"]}>Date</span>
+					<span className={styles["rating"]}>Rating</span>
+				</div>
+
+				<ul className={styles["entry-list"]}>
+					<li className={styles["entry-item"]}>
+						<span className={styles["name"]}>Cafe Grumpy</span>
+						<span className={styles["status"]}>Visited</span>
+						<span className={styles["address"]}>
+							193 Meserole Ave, Brooklyn, NY 11222
+						</span>
+						<span className={styles["date"]}>4/13/2015</span>
+						<span className={styles["rating"]}>
+							<Star />
+							<Star />
+							<Star />
+							<Star />
+							<Star />
+						</span>
+					</li>
+				</ul>
 			</details>
 		</section>
 	);
