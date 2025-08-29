fcaba32a87935ebaf095325235dd8b44c005f6c9
Derick Moncado <derickmoncado@gmail.com>
2025-08-29 16:29:07 -0700

    Broke out the Entry component into it's own thing.

--- src/components/entries/entries.tsx
+++ src/components/entries/entries.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./entries.module.scss";
-import { Star } from "lucide-react";
+import Entry from "../entry/entry";
 
 export default function Entries() {
 	return (
@@ -19,21 +19,7 @@ export default function Entries() {
 				</div>
 
 				<ul className={styles["entry-list"]}>
-					<li className={styles["entry-item"]}>
-						<span className={styles["name"]}>Bodega Park</span>
-						<span className={styles["status"]}>Favorite</span>
-						<span className={styles["address"]}>
-							2852 W Sunset Blvd, Los Angeles, CA 90026
-						</span>
-						<span className={styles["date"]}>4/15/2025</span>
-						<span className={styles["rating"]}>
-							<Star />
-							<Star />
-							<Star />
-							<Star />
-							<Star />
-						</span>
-					</li>
+					<Entry />
 				</ul>
 			</details>
 		</section>
