7d3ce38399c102862ec8601f653a54b2c64a92dd
Derick Moncado <derickmoncado@gmail.com>
2025-08-29 17:27:57 -0700

    backfilled "status" styles for the Entry

--- src/components/entry/entry.tsx
+++ src/components/entry/entry.tsx
@@ -4,20 +4,22 @@ import { Star } from "lucide-react";
 
 export default function Entry() {
 	return (
-		<li className={styles["entry"]}>
-			<span className={styles["name"]}>Bodega Park</span>
-			<span className={styles["status"]}>Favorite</span>
-			<span className={styles["address"]}>
-				2852 W Sunset Blvd, Los Angeles, CA 90026
-			</span>
-			<span className={styles["date"]}>4/15/2025</span>
-			<span className={styles["rating"]}>
-				<Star />
-				<Star />
-				<Star />
-				<Star />
-				<Star />
-			</span>
-		</li>
+		<>
+			<li className={`${styles["entry"]} ${styles["favorite"]}`}>
+				<span className={styles["name"]}>Bodega Park</span>
+				<span className={styles["status"]}>Favorite</span>
+				<span className={styles["address"]}>
+					2852 W Sunset Blvd, Los Angeles, CA 90026
+				</span>
+				<span className={styles["date"]}>4/15/2025</span>
+				<span className={styles["rating"]}>
+					<Star />
+					<Star />
+					<Star />
+					<Star />
+					<Star />
+				</span>
+			</li>
+		</>
 	);
 }
